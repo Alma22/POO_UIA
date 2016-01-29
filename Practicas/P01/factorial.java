f37daccaacfe318a8833e1717deac014ab5c77a8
ALMA ZARATE <perla2211@gmail.com>
2016-01-29 03:16:54 -0600

    practica1

---
+++ Practicas/P01/factorial.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class  archivo
+{
+  public static void main (String arg []) throws IOException
+   {int n,a,x;
+BufferedReader dat=new BufferedReader (new InputStreamReader(System.in));
+  do{
+     System.out.print("Ingrese el numero: ");
+	n=Integer.parseInt(dat.readLine());
+	a=n;x=n;
+	if(n>33){System.out.println("ingrese un numero menor a 34");}
+    }while(a>33);
+  do {n=n-1;
+      a=a*n;
+     }while(n>1);
+	
+	System.out.println("la factorial de "+x+" es: " +a);
+   }
+}
