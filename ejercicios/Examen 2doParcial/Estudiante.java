e1782bb130f06d5259484d6098c76e9282b2eb73
ALMA ZARATE <perla2211@gmail.com>
2016-05-03 21:48:16 -0500

    2do Parcial

---
+++ ejercicios/Examen 2doParcial/Estudiante.java
@@ -0,0 +1,53 @@
+public class Estudiante{
+
+	String NoEstudiante;
+	String Nombre;
+	Fecha FechaNacimiento;
+	int Puntuacion;
+
+	private Estudiante(String NoEstudiante, String Nombre, Fecha FechaNacimiento,int Puntuacion){
+
+		NoEstudiante = iNoEstudiante;
+		Nombre = iNombre;
+		FechaNacimiento = iFechaNacimiento;
+		Puntuacion = iPuntuacion;
+
+   
+  }
+  public void setNoEstudiante(String iNoEstudiante){
+	NoEstudiante = iNoEstudiante;
+}
+  public String getNoEstudiante(){
+	return NoEstudiante;
+}
+
+  public void setNombre(String iNombre){
+	Nombre = iNombre;
+}
+
+  public String getNombre(){
+	return Nombre;
+}
+ public void setFechaNAcimiento(Fecha iFechaNacimiento){
+ 	FechaNacimiento = iFechaNacimiento;
+ }
+ public Fecha getFechaNacimiento(){
+ 	return FechaNacimiento;
+ }
+
+ public void setPuntuacion(int iPuntuacion){
+ 	Puntuacion = iPuntuacion;
+ }
+ public int getPuntuacion(){
+ 	return Puntuacion;
+ }
+
+ public int Puntuacion{
+
+ 	for ( i = )
+ }
+
+
+
+
+}
